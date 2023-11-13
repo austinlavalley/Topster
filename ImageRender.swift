3fad6937f99288d0f7185e85c609e1d87e29d492
austinlavalley <hello@austinlavalley.com>
2023-11-13 17:22:04 -0600

    totally rebuilt snapshot ability + correct implementation of share sheet

--- ImageRender.swift
+++ ImageRender.swift
@@ -7,90 +7,61 @@
 
 import SwiftUI
 
-// An example view to render
+
+
 struct RenderView: View {
-    let image: Image
     let album: Album
-
+    
+    @State private var snapshot: UIImage?
+    
     var body: some View {
-//        VStack {
-            Text(album.artist)
-            Text(album.name)
-            Text(album.image.first(where: { $0.size == "large"})?.text ?? "").frame(width: 360)
-            
-            AsyncImage(url: URL(string: album.image.first(where: { $0.size == "large" })?.text ?? "")) { phase in
-                switch phase {
-                case .empty:
-                    Color.purple.opacity(0.1)
-                case .success(let image):
-                    image
-                        .resizable()
-                        .scaledToFill()
-                case .failure(_):
-                    Image(systemName: "exclamationmark.icloud")
-                        .resizable()
-                        .scaledToFit()
-                @unknown default:
-                    Image(systemName: "exclamationmark.icloud")
-                }
+        VStack(spacing: 24) {
+            if let image = snapshot {
+                Image(uiImage: image)
+                
+                ShareLink(
+                    item: Image(uiImage: snapshot!),
+                    preview: SharePreview(album.name, image: Image(uiImage: snapshot!), icon: sharePreview)
+                )
             }
-            .frame(width: 240, height: 240)
-            .cornerRadius(20)
-                        
-            image
-                .frame(width: 240, height: 240)
-                .cornerRadius(20)
-//        }
+            
+            InternetImage(url: "https://lastfm.freetls.fastly.net/i/u/300x300/07c3b7f594f5513c5f07fa7f8fb81787.png") { image in
+                image
+                    .resizable()
+                    .frame(width: 240, height: 240)
+                    .cornerRadius(24)
+            }
+            
+            Button(action: generateSnapshot) {
+                Text("Create snapshot")
+            }.buttonStyle(.bordered)
+        }
+    }
+    
+    var sharePreview: some Transferable {
+        Image(systemName: "text.book.closed.fill")
     }
 }
 
 
-struct ImageRender: View {
-    
-    @State private var image = Image("localAlbum")
-    
-    @State private var renderedImage = Image(systemName: "photo")
-    @Environment(\.displayScale) var displayScale
-
-    var body: some View {
-        VStack {
-            
-            // what being displayed is actually the rendered image, the RenderView doesn't actually exist
-            renderedImage
-
-            // RENDERVIEW IS WORKING FINE, IT'S THE IMAGERENDERER THAT CANNOT CONVERT THE ASYNCIMAGES INTO AN IMAGE
-            
-            
-            // export really just means pulling up the share menu on the displayed image
-            ShareLink("Export", item: renderedImage, preview: SharePreview(Text("Shared image"), image: renderedImage))
-
-            
-            Button("re render") { render() }
-            
-        }
-        
-        // on first view OR when it is changed, rerender the image
-//        .onChange(of: text) { _ in render() }
-        .onAppear { render() }
-    }
-
-    
-    
-    // func for rendering
-    @MainActor func render() {
-        let renderer = ImageRenderer(content: RenderView(image: image, album: Album(name: "American Heartbreak", artist: "Zach Bryan", url: "https://www.last.fm/music/Zach+Bryan/American+Heartbreak", image: [Topster.AlbumImage(text: Optional("https://lastfm.freetls.fastly.net/i/u/34s/07c3b7f594f5513c5f07fa7f8fb81787.png"), size: Optional("small")), Topster.AlbumImage(text: Optional("https://lastfm.freetls.fastly.net/i/u/64s/07c3b7f594f5513c5f07fa7f8fb81787.png"), size: Optional("medium")), Topster.AlbumImage(text: Optional("https://lastfm.freetls.fastly.net/i/u/174s/07c3b7f594f5513c5f07fa7f8fb81787.png"), size: Optional("large")), Topster.AlbumImage(text: Optional("https://lastfm.freetls.fastly.net/i/u/300x300/07c3b7f594f5513c5f07fa7f8fb81787.png"), size: Optional("extralarge"))], streamable: "0", mbid: "")))
-
-        // make sure and use the correct display scale for this device
-        renderer.scale = displayScale
-
-        if let uiImage = renderer.uiImage {
-            renderedImage = Image(uiImage: uiImage)
+extension RenderView {
+    func generateSnapshot() {
+        Task {
+            let renderer = await ImageRenderer(content: RenderView(album: Album(name: "American Heartbreak", artist: "Zach Bryan", url: "https://www.last.fm/music/Zach+Bryan/American+Heartbreak", image: [Topster.AlbumImage(text: Optional("https://lastfm.freetls.fastly.net/i/u/34s/07c3b7f594f5513c5f07fa7f8fb81787.png"), size: Optional("small")), Topster.AlbumImage(text: Optional("https://lastfm.freetls.fastly.net/i/u/64s/07c3b7f594f5513c5f07fa7f8fb81787.png"), size: Optional("medium")), Topster.AlbumImage(text: Optional("https://lastfm.freetls.fastly.net/i/u/174s/07c3b7f594f5513c5f07fa7f8fb81787.png"), size: Optional("large")), Topster.AlbumImage(text: Optional("https://lastfm.freetls.fastly.net/i/u/300x300/07c3b7f594f5513c5f07fa7f8fb81787.png"), size: Optional("extralarge"))], streamable: "0", mbid: "")))
+            if let image = await renderer.uiImage {
+                self.snapshot = image
+            }
         }
     }
 }
+
+
+
+
+
 
 struct ImageRender_Previews: PreviewProvider {
     static var previews: some View {
-        ImageRender()
+        RenderView(album: Album(name: "American Heartbreak", artist: "Zach Bryan", url: "https://www.last.fm/music/Zach+Bryan/American+Heartbreak", image: [Topster.AlbumImage(text: Optional("https://lastfm.freetls.fastly.net/i/u/34s/07c3b7f594f5513c5f07fa7f8fb81787.png"), size: Optional("small")), Topster.AlbumImage(text: Optional("https://lastfm.freetls.fastly.net/i/u/64s/07c3b7f594f5513c5f07fa7f8fb81787.png"), size: Optional("medium")), Topster.AlbumImage(text: Optional("https://lastfm.freetls.fastly.net/i/u/174s/07c3b7f594f5513c5f07fa7f8fb81787.png"), size: Optional("large")), Topster.AlbumImage(text: Optional("https://lastfm.freetls.fastly.net/i/u/300x300/07c3b7f594f5513c5f07fa7f8fb81787.png"), size: Optional("extralarge"))], streamable: "0", mbid: ""))
     }
 }
---
+++ InternetImage.swift
@@ -0,0 +1,67 @@
+//
+//  InternetImage.swift
+//  Topster
+//
+//  Created by Austin Lavalley on 11/13/23.
+//
+
+import SwiftUI
+
+struct InternetImage<Content: View>: View {
+    var url: String
+    
+    @State private var image: UIImage?
+    @State private var errors: String?
+
+    @ViewBuilder var content: (Image) -> Content
+    
+    init(url: String, @ViewBuilder content: @escaping (Image) -> Content) {
+        self.url = url
+        self.content = content
+    }
+    
+
+    var body: some View {
+        VStack {
+            if let image = image {
+                content(Image(uiImage: image))
+            } else {
+                ProgressView().onAppear { loadImage() }
+            }
+        }
+    }
+    
+    private func loadImage() {
+        guard let url = URL(string: url) else {
+            return
+        }
+        
+        let urlRequest = URLRequest(url: url)
+        
+        if let cachedResponse = URLCache.shared.cachedResponse(for: urlRequest),
+           let image = UIImage(data: cachedResponse.data) {
+            self.image = image
+        } else {
+            URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+                if let data = data, let response = response, let image = UIImage(data: data) {
+                    let cachedResponse = CachedURLResponse(response: response, data: data)
+                    URLCache.shared.storeCachedResponse(cachedResponse, for: urlRequest)
+                    DispatchQueue.main.async {
+                        self.image = image
+                    }
+                }
+            }.resume()
+        }
+    }
+}
+
+struct InternetImage_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
+        InternetImage(url: "https://lastfm.freetls.fastly.net/i/u/174s/62ee1cffdde64d1e9a3462c307f83bfd.png") { image in
+            image
+                .resizable()
+        }
+    }
+}
