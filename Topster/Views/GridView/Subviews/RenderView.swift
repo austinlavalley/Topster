a936c3c87994f782f7c3bd983d881e96961d3c4c
austinlavalley <hello@austinlavalley.com>
2024-04-24 17:56:00 -0500

    pushing a stupid simple fix for the export view not loading on first view. not a permanent fix, but just forcing the generate function to run a second time onAppear after a short delay is a decent fix for now

--- Topster/Views/GridView/Subviews/RenderView.swift
+++ Topster/Views/GridView/Subviews/RenderView.swift
@@ -24,7 +24,8 @@ struct RenderView: View {
 
             VStack(spacing: 24) {
                 
-                HStack(spacing: 10) {
+                // THIS WORKS, AND LOADS ALL IMAGES ON FIRST LOAD OF RENDERVIEW
+                HStack(spacing: 40) {
                     ForEach(vm.FortyGridDict.sorted(by: { $0.key < $1.key }).prefix(5).dropFirst(0), id: \.key) { key, album in
                         if album != nil {
                             AlbumSquare(album: album!)
@@ -41,11 +42,15 @@ struct RenderView: View {
                 
     // so the issue is the view being displayed is just a static image of the renderview BEFORE it can fetch the async images. due to it being static, obviously the images won't load in because it takes a nonzero amount of time to fetch them. need to convert this to actually show the live view, and then do the rendering of the snapshot when exported
                 
+                // we can't simply jsut build the snapshot view in the BG and use the ForEach solution because when we go to export, even though it's in the BG it still hasn't fetched the images until we refresh. So how do we either put a delay on the imageRender, or use some sort of await/onchange logic to alert when it's loaded?
+                
                 // snapshot of grid to export
                 if let image = snapshot {
                     Image(uiImage: image)
                         .resizable()
                         .scaledToFit()
+                    
+                    
                     
                     // export buttons
                     VStack {
@@ -96,9 +101,9 @@ struct RenderView: View {
         .onAppear {
             generateSnapshot()
 
-//            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-//                showLoading = false
-//            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                generateSnapshot()
+            }
         }
         
 
@@ -112,12 +117,14 @@ struct RenderView: View {
 
 
 // the imagerenderer works immediately, what we need to do is figure out how to await the isoloated FortyGridExportView to give it time to fetch all images
+//
+// idk what i was on about above, but this generates the exported image. the failure is happening because the generation is happneing before images can be fetched
 extension RenderView {
     func generateSnapshot() {
         Task {
             let renderer = ImageRenderer(content:
                 FortyGridExportView()
-                .environmentObject(vm)
+                    .environmentObject(vm)
             )
             
             if let image = renderer.uiImage {
