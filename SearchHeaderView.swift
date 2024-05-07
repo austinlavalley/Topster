19d44e0f0e1bb1923bf81f8d2b6c7bc1e1ef1414
austinlavalley <hello@austinlavalley.com>
2024-05-06 23:28:42 -0500

    started rebuilding the search sheet to show current album selected, if one

---
+++ SearchHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchHeaderView.swift
+//  Topster
+//
+//  Created by Austin Lavalley on 5/6/24.
+//
+
+import SwiftUI
+
+struct SearchHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchHeaderView()
+}
